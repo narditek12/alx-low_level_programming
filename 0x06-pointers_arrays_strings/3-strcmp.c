93835a1a3afdf13a4f925416be5c068877cbbd78
Nardos <narditek1@gmail.com>
2022-03-24 15:01:30 +0300

    Initial Commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,23 +1,24 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * _strcmp - compares two strings
- * @s1: first string.
- * @s2: second string.
- * Return: 0 if s1 and s2 are equals,
- * another number if not.
+ *
+ * @s1: char to check
+ * @s2: char to check
+ * Return: 0 is success
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, op = 0;
-
-	while (op == 0)
-	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-			break;
-		op = *(s1 + i) - *(s2 + i);
-		i++;
-	}
-
-	return (op);
+while ((*s1 != '\0' && *s1 != '\0') && *s1 == *s2)
+{
+s1++;
+s2++;
+}
+if (*s1 == *s2)
+{
+return (0);
+}
+else
+{
+return (*s1 - *s2);
+}
 }
